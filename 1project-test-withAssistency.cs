9307596bd67f5ec5b8a3bcf54b7d51d335a98b39
DreamFXX <filipek.husak@gmail.com>
2024-10-24 20:21:36 +0200

    Created empty .cs file for my first test from ms learn.

---
+++ 1project-test-withAssistency.cs
@@ -0,0 +1,4 @@
+- // First project that i did by exercising CSharp, doing tasks on MS Learn.
+- // https://learn.microsoft.com/cs-cz/training/modules/guided-project-calculate-print-student-grades/1-introduction
+
+
