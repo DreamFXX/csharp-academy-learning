8f9f46ea29740ea739f5adf23794d40d2768116d
DreamFXX <121397930+DreamFXX@users.noreply.github.com>
2024-10-24 18:11:21 +0200

    Create Variables_dataTypes_example.cs

---
+++ Variables_dataTypes_example.cs
@@ -0,0 +1,14 @@
+- Sample work with variables and data types
+
+
+string name = "Robert";
+int num = 3;
+decimal point = 34.4m; // Decimal or Float or double
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(num);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(point);
+Console.Write(" celsius.");
